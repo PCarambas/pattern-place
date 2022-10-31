05f94bd201460e0478d3f39c257c9ccc50f16798
Peggy Carambas <PCarambas@gmail.com>
2022-10-31 15:27:12 -0500

    Added form and other modules

---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,55 @@
+import { Route, Routes, Outlet } from "react-router-dom"
+import { PatternPlace } from "../PatternPlace"
+import { Patterns } from "../patterns/Patterns"
+import { PatternForm } from "../PatternForm"
+import { PatternDetails } from "../details/PatternDetails"
+import { PatternEdit } from "../users/PatternEdit"
+import { useNavigate } from "react-router-dom"
+
+export const ApplicationViews = () => {
+
+	const navigate = useNavigate()
+
+	const navigateToPatterns = () => {
+		navigate('/Patterns')
+	}
+
+	const navigateToPatternForm = () => {
+		navigate('/PatternForm')
+	}
+	return <>
+		<Routes>
+			<Route path="/" element={
+				<>
+					
+						<h1>Pattern Place</h1>
+						<h3>Keep calm and sew on</h3>
+					
+
+					<Outlet />
+
+					<div>
+						<button onClick={navigateToPatterns}>Select A Pattern</button>
+					</div>
+
+					<div>
+						<button onClick={navigateToPatternForm}>Add A Pattern</button>
+					</div>
+				</>
+
+			} >
+				<Route path=":patternId" element={<PatternDetails />} />
+				<Route index path="PatternPlace" element={<PatternPlace />} />
+				<Route path="Patterns" element={<Patterns />} />
+				<Route path="PatternForm" element={<PatternForm />} />
+				
+
+
+			</Route>
+			
+		</Routes>
+		
+	
+	</>
+
+}
